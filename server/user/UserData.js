63b92ece1d709fc2d2d2e531f3140f79c1ee0045
Micheloncio <escolano_garnica@hotmail.com>
2018-02-15 12:13:27 +0100

    add validating vote and reset votes

--- server/user/UserData.js
+++ server/user/UserData.js
@@ -20,5 +20,23 @@ class UserData{
             .catch(reject)
         })
     }
+    changeVoteToTrue(name){
+        return new Promise((resolve,reject)=>{
+            if(!name)
+            throw new Error('no name provided')
+
+            User.update({name},{voted:true})
+                .then(resolve)
+                .catch(reject)
+        })
+    }
+    resetVotes(){
+        return new Promise((resolve,reject)=>{
+
+            User.update({},{voted:false},{multi: true})
+                .then(resolve)
+                .catch(reject)
+        })
+    }
 }
 module.exports = UserData
--- server/vote/VoteLogic.js
+++ server/vote/VoteLogic.js
@@ -7,26 +7,32 @@ class VoteLogic{
     }
     
     addVote(name, vote1, vote2, vote3){
-        return checkUser(name)
+        return checkUserAndVote(name)
             .then(res =>{
                 if(res){
                     const _name = name.toLowerCase()
                     const _vote1 = vote1.toLowerCase()
                     const _vote2 = vote2.toLowerCase()
                     const _vote3 = vote3.toLowerCase()
+                    userLogic.changeVoteToTrue(name)
                     return voteData.addVote(_name, _vote1, _vote2, _vote3)
                 }else{
-                    throw new Error('name invalid!')
+                    throw new Error('name invalid or this user has already voted!')
                 }
             })
     }
 }
-
-function checkUser(name){
+function checkUserAndVote(name){
     return userLogic.listUsers()
-            .then(users=>{
-                return users.some(user=> name.toLowerCase()===user.name)
-            })
+        .then(users=>(checkUser(users,name) && !checkVote(users,name)))
+}
+
+function checkUser(users,name){
+    return users.some(user=> name.toLowerCase()===user.name)
+}
+function checkVote(users,name){
+    const arrFiltered = users.filter(user=>name.toLowerCase()===user.name)
+    return arrFiltered[0].voted
 }
 function filterVotes(votes){
     const filteredVotes = []
